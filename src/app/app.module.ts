23348753110e907d5ee5ec6371a6683dd583fff4
HugoRbrDev <hugorbrdev@gmail.com>
2024-04-05 11:01:17 -0300

    criados os componentes header, footer, banner e card. E criada uma pasta "componentes" dentro de "app", para separar os componentes que serão compartilhados. Instalado AngularMaterial 16

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
+import { BannerComponent } from './components/shared/banner/banner.component';
+import { CardComponent } from './components/shared/card/card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BannerComponent,
+    CardComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
